1114215a00580d42de6568f3813ae18d7a1a3b05
pacosixowe <pacosixowe@gmail.com>
2025-08-21 00:34:19 -0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to PrimeOffers 🚀</h1>
+      <p>Your new storefront + content site is live!</p>
+    </div>
+  );
+}
